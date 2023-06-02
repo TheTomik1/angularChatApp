76a90f1cf2a4c9669c8436a2dbc4c21f3d460c18
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 17:03:58 +0200

    4 files modified

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent {
+  users = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.getUsersData();
   }
 
+  getUsersData(): void {
+    this.http.get<any[]>('https://dummyjson.com/users').subscribe((response) => {
+        for (let user of response["users"]) {
+          this.users.push({firstName: user.firstName, lastName: user.lastName, email: user.email});
+        }
+      },
+      (error) => {
+        console.error('Error fetching user data:', error);
+      }
+    );
+  }
 }
