0db30e989bc55d947c66e5237ae9cc72030e4999
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 09:15:07 +0200

    4 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   imports: [AppRoutingModule],
-  declarations: [LoginComponentComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponentComponent } from './login-component/login-component.component';
-
 const routes: Routes = [
-  { path: '/login', component: LoginComponentComponent }
 ];
 
 @NgModule({
