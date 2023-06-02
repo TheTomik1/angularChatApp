d0b086ba35ffd77106aeb97ef7b8108312419fb0
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 16:16:18 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+
+import { LoggedInGuard } from './logged.guard.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [AppComponent],
-  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [AppComponent, LoggedInGuard, AuthService],
+  declarations: [AppComponent, LoginComponent, ChatComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
