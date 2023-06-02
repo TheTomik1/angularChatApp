9c611ce9c77005f407670ffdc94c9f31ffc88350
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 13:54:34 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [AppComponent],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
