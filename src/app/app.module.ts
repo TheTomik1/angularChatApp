63caa4a9f8ebe3e31b9dc97a117470a56dcac5cd
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 09:40:13 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  imports: [AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [AppComponent],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
