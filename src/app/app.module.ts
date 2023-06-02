53b8c352a952f670b885c7bca225c95fba190406
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 08:44:08 +0200

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app.routing.module';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [AppRoutingModule],
+  declarations: [LoginComponentComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
