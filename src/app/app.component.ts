1486f1fa2f3f6deb51a9790af56cd093ffe2e785
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-02 17:20:17 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './auth.service';
+
 @Component({
   selector: 'chat-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private authService: AuthService) {}
+
+  isLoggedIn = this.authService.logggedInStatus;
 }
