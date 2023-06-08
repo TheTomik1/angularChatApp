1d2b023ce428d24449e4b633d0f6d77fff72d797
TheTomik1 <48056564+TheTomik1@users.noreply.github.com>
2023-06-08 12:00:53 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'chat-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private authService: AuthService) {}
-
-  isLoggedIn = this.authService.logggedInStatus;
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.navigate(["/login"]);
+  }
 }
